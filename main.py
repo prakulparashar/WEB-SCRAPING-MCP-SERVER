33331bb839ae791d250d09a3584882cb66038f7d
prakulparashar <prakulparashar06@gmail.com>
2026-06-17 00:51:48 +0530

    intial

---
+++ main.py
@@ -0,0 +1,29 @@
+import http.client
+import json
+import os
+import asyncio #mandatory library to use async fxns in python
+import httpx #needed when making async api calls, this library basically executes aynschronous https requests
+from dotenv import load_dotenv
+load_dotenv()
+
+query = "chromadb"
+SERPER_URL = "https://google.serper.dev/search"
+
+async def web_search(query:str) -> dict | None:
+    payload = json.dumps({"q": query, "num":2})
+    headers = {
+    'X-API-KEY': os.getenv("SERPER_API_KEY"),
+    'Content-Type': 'application/json'
+    }
+
+    
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            SERPER_URL, headers=headers, data=payload, timeout=30.0
+        )
+        response.raise_for_status()
+        return response.json()
+
+res = asyncio.run(web_search(query="Chroma DB"))
+print(res)
+
