d09570d6c4607f984cb0000150ad0d46d0957491
prakulparashar <prakulparashar06@gmail.com>
2026-06-19 15:55:31 +0530

    updated

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import os
 import asyncio #mandatory library to use async fxns in python
 import httpx #needed when making async api calls, this library basically executes aynschronous https requests
 from dotenv import load_dotenv
+from utils import clean_html_to_text
 load_dotenv()
 
-query = "chromadb"
+
 SERPER_URL = "https://google.serper.dev/search"
 
 async def web_search(query:str) -> dict | None:
@@ -18,12 +19,50 @@ async def web_search(query:str) -> dict | None:
 
     
     async with httpx.AsyncClient() as client:
-        response = await client.post(
-            SERPER_URL, headers=headers, data=payload, timeout=30.0
-        )
+        response = await client.post(SERPER_URL, headers=headers, data=payload, timeout=30.0)
         response.raise_for_status() #only executes if there is error, otherwise doesn't run 
         return response.json()
 
-res = asyncio.run(web_search(query="Chroma DB"))
-print(res)
 
+#search the req. URL and clean its HTML to get only the text
+async def fetch_url(url:str):
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, timeout= 30.0)
+        cleaned_response = clean_html_to_text(response.text)
+        return cleaned_response
+
+
+#main MCP tool
+docs_urls = {
+    "langchain": "python.langchain.com/docs",
+    "llama-index": "docs.llamaindex.ai/en/stable",
+    "openai": "platform.openai.com/docs",
+    "uv": "docs.astral.sh/uv",
+}
+
+async def get_docs(query:str, library:str):
+    if library not in docs_urls:
+        return "no result found"
+    
+    query = f"site:{docs_urls[library]} {query}"
+
+    results = await web_search(query)
+
+    if len(results["organic"]) == 0:
+        return "no result found"
+    
+    text_parts = []
+    for result in results:
+        link = result.get("link", "")
+
+        raw = await fetch_url(link)
+        if raw:
+            labeled = f"source: {link}/n{raw}" 
+            text_parts.append(labeled)
+
+        return "/n/n".join(text_parts)
+    
+
+
+
+    
