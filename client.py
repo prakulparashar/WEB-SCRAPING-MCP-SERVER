857a891611ef33827c470ed9a3a02a89c9a03bc1
prakulparashar <prakulparashar06@gmail.com>
2026-06-20 14:32:53 +0530

    updated

--- client.py
+++ client.py
@@ -20,18 +20,43 @@ async def main():
 
     async with stdio_client(server_params) as (read_stream, write_stream):
         async with ClientSession(read_stream, write_stream) as session:
+
             await session.initialize()
 
             tools_response = await session.list_tools()
             print("Available tools:", [t.name for t in tools_response.tools])
 
+            query = "How to use publish a package with uv on gitlab"
+            library = "uv"
+            res = await session.call_tool("get_docs", arguments = {"query": query, "library": library})
 
+            context = res.content
+            user_prompt_with_context = f"Query: {query}, Context {context}"
 
-
-
-
-
-
+            # LLM function to create a human readable response
+            SYSTEM_PROMPT= """
+            Answer ONLY using the provided context. If info is missing say you don't know.
+            Keep every 'SOURCE:' line exactly; list sources at the end.
+            """
+            answer = get_response_from_llm(user_prompt=user_prompt_with_context, system_prompt=SYSTEM_PROMPT, model="openai/gpt-oss-20b")
+            print("ANSWER: ", answer)
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
